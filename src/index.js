fef5c7ebae468a09443679f06a9f0aed3fada0d8
saroltah <saroltah@users.noreply.github.com>
2023-03-01 19:41:53 +0100

    changed some cities and made select working

--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
 function updateTime() {
-  let tokyo = document.querySelector("#tokyo");
-  let tokyodate = tokyo.querySelector(".date");
-  let tokyotime = tokyo.querySelector(".time");
-  tokyodate.innerHTML = moment().format("MMMM D, YYYY");
-  tokyotime.innerHTML = moment().tz("Asia/Tokyo").format("HH:mm:ss");
-
-  let budapest = document.querySelector("#budapest");
-  let budapestdate = budapest.querySelector(".date");
-  let budapesttime = budapest.querySelector(".time");
-  budapestdate.innerHTML = moment().format("MMMM D, YYYY");
-  budapesttime.innerHTML = moment().tz("Europe/Budapest").format("HH:mm:ss");
+  if (tokyo) {
+    let tokyo = document.querySelector("#tokyo");
+    let tokyodate = tokyo.querySelector(".date");
+    let tokyotime = tokyo.querySelector(".time");
+    tokyodate.innerHTML = moment().format("MMMM D, YYYY");
+    tokyotime.innerHTML = moment().tz("Asia/Tokyo").format("HH:mm:ss");
+  }
+  if (dublin) {
+    let dublin = document.querySelector("#dublin");
+    let dublindate = dublin.querySelector(".date");
+    let dublintime = dublin.querySelector(".time");
+    dublindate.innerHTML = moment().format("MMMM D, YYYY");
+    dublintime.innerHTML = moment().tz("Europe/Dublin").format("HH:mm:ss");
+  }
+  if (cairo) {
+    let cairo = document.querySelector("#cairo");
+    let cairodate = cairo.querySelector(".date");
+    let cairotime = cairo.querySelector(".time");
+    cairodate.innerHTML = moment().format("MMMM D, YYYY");
+    cairotime.innerHTML = moment().tz("Africa/Cairo").format("HH:mm:ss");
+  }
 }
-
 setInterval(updateTime, 1000);
+
+let selectCities = document.querySelector("#cities");
+selectCities.addEventListener("change", showCity);
+let openingPage = document.querySelector("#opening-page");
+
+function showCity(event) {
+  let timeZone = event.target.value;
+  let cityTime = moment().tz(timeZone).format("HH:mm:ss");
+  let cityDate = moment().format("MMMM D, YYYY");
+  let cityName = timeZone.replace("_", " ").split("/")[1];
+  openingPage.innerHTML = `<div class="city">
+          <div>
+            <h2 class="cityname">${cityName}</h2>
+            <div class="date">${cityDate}</div>
+          </div>
+          <div>
+            <div class="time">${cityTime}</div>
+          </div>
+          </div>
+           <div><a href="index.html" class="more-cities-link">More cities</a></div>`;
+}
